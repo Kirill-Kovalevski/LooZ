a042859d823af22d45f2d62d656a0f985cf876ca
Kirill Kovalevski <kirill.kovalevski@mail.huji.ac.il>
2025-10-14 16:27:57 +0300

    Deploy via Actions (build dist to Pages)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 
-// If deploying to https://<user>.github.io/<repo>/ set base to '/<repo>/'
-const base = process.env.GH_PAGES ? '/LooZ/' : '/';
-
 export default defineConfig({
-  base,
+  base: '/LooZ/',            // <-- hardcode to be safe
   server: { port: 5173, open: true }
 });
